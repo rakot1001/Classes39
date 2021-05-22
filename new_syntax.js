cfd2206f9a5a340f26d3d9541b46764753061a78
Vlad <vladdp70@gmail.com>
2021-05-22 23:49:12 +0300

    Свой метод зарплаты за все дни

--- new_syntax.js
+++ new_syntax.js
@@ -10,8 +10,8 @@ class UserClass {
   getFullName() {
     return `${this.name} ${this.surName}`;
   }
-  IsAdult(){
-    if(this.age>=18){
+  IsAdult() {
+    if (this.age >= 18) {
       return `${this.name} is ${this.age} old and he is adult`;
     }
     return false;
@@ -20,3 +20,22 @@ class UserClass {
 
 const user1 = new UserClass("User1", "Test", 21);
 const user2 = new UserClass("User2", "Test", 17);
+
+/*Реализовать класс Рабочий(Worker)
+У рабочего есть имя. фамилия, ставка за день, количество отработаных дней
+Метод, который возвращает з.п рабочего
+*/
+class Worker {
+  constructor(name, surName, moneyForDay, workDays) {
+    this.name = name;
+    this.surName = surName;
+    this.moneyForDay = moneyForDay;
+    this.workDays = workDays;
+  }
+
+  getMoneyForAllDays() {
+    return `${this.moneyForDay*this.workDays} this worker have`;
+  }
+  
+}
+const worker1 = new Worker('Worker1','W1',120,10);
