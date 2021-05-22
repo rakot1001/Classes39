1d3d2fb82c82394952cd945d815453621aad128e
Vlad <vladdp70@gmail.com>
2021-05-22 23:09:04 +0300

    Создал MyArray

---
+++ MyArray.js
@@ -0,0 +1,15 @@
+// Объект с логикой
+
+function MyArrayProto() { 
+}
+// Объекты с данными
+function MyArray(...args){
+  
+}
+
+MyArray.isMyArray = function isMyArray(obj){
+  return obj instanceof MyArray;
+}
+
+MyArray.prototype = new MyArrayProto();
+
