924ab21dce77033a26f7d33b0124fc3555b9b8f7
Vlad <vladdp70@gmail.com>
2021-05-22 23:40:29 +0300

    IsAdult свой

--- new_syntax.js
+++ new_syntax.js
@@ -10,6 +10,13 @@ class UserClass {
   getFullName() {
     return `${this.name} ${this.surName}`;
   }
+  IsAdult(){
+    if(this.age>=18){
+      return `${this.name} is ${this.age} old and he is adult`;
+    }
+    return false;
+  }
 }
 
-const uClass = new UserClass("classUser1", "Test", 21);
+const user1 = new UserClass("User1", "Test", 21);
+const user2 = new UserClass("User2", "Test", 17);
